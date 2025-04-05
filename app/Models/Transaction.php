ad4b1077bcb298331c0c319e675fb28888f135b8
tuantq <kutuanonline199@gmail.com>
2025-04-05 22:32:51 +0700

    cast money

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cknow\Money\Casts\MoneyIntegerCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +15,6 @@ class Transaction extends Model
 
     public $casts = [
         'date' => 'datetime',
+        'amount' => MoneyIntegerCast::class,
     ];
 }
